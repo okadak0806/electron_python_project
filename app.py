b7a18935241659c3463163c0353cd680a7007e59
Kazumi Okada <okadak@sci.hokudai.ac.jp>
2020-12-08 15:55:04 +0900

    first step end

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def hello():
     return render_template("index.html")
 
 
-# if __name__ == "__main__":
-#     print('on hello')
-#     app.run(host="127.0.0.1", port=5000)
+if __name__ == "__main__":
+    print('on hello')
+    app.run(host="127.0.0.1", port=5000)
